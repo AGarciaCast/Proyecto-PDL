53a151b1209648b09ee9278bd7c9b80dc20d1e14
popi300 <33000879+popi300@users.noreply.github.com>
2019-10-06 19:48:57 +0200

    UPDATE Cuerpo A.Lex

--- Compilador.java
+++ Compilador.java
@@ -3,22 +3,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Set;
-import java.util.TreeSet;
 
-public class Compilador {
-	char car;
-	final MatrizTransiciones MT_AFD = new MatrizTransiciones();
+public  class Compilador {
+	static char car;
+	static MatrizTransiciones MT_AFD = new MatrizTransiciones();
 	static BufferedReader br;
-	//Para las acciones semánticas
-	int num = 0;
-	String lex = "";
-	//TODO: rellenar con las palabras reservadas
-	Set<String> palRes = new Set<String>();
-	
+	//Para las acciones semanticas
+	static int num = 0;
+	static String lex = "";
 	
 	//Contenido de la MatrizTransiciones
-	public class ParEstadoAccion {
+	private static class ParEstadoAccion {
 		private int estado;
 		private String accion;
 		
@@ -36,7 +31,7 @@ public class Compilador {
 		}
 	}
 	
-	public class MatrizTransiciones{
+	public static class MatrizTransiciones{
 		
 		/*
 		 * Filas: 0 - 21 (a partir del 8 son estados finales)
@@ -57,9 +52,9 @@ public class Compilador {
 		public MatrizTransiciones() {
 			this.matriz = new ParEstadoAccion[22][19]; 
 			
-			//del -> ' ' , l -> 'l' , d -> '1' , c -> '.'
+			//del -> ' ' , l -> 'a' , d -> '1' , c -> '.'
 			matriz[0][char2int('|')] = new ParEstadoAccion(1, "lee");
-			matriz[0][char2int('l')] = new ParEstadoAccion(2, "C");
+			matriz[0][char2int('a')] = new ParEstadoAccion(2, "C");
 			matriz[0][char2int('1')] = new ParEstadoAccion(3, "A");
 			matriz[0][char2int('\'')] = new ParEstadoAccion(4, "lee");
 			matriz[0][char2int('/')] = new ParEstadoAccion(5, "lee");
@@ -75,7 +70,7 @@ public class Compilador {
 			
 			//Todas las que no son l, d, _, son o.c.
 			for (int i = 0; i < 19; i++) matriz[2][i] = new ParEstadoAccion(9, "G2");
-			matriz[2][char2int('l')] = new ParEstadoAccion(2, "C");
+			matriz[2][char2int('a')] = new ParEstadoAccion(2, "C");
 			matriz[2][char2int('1')] = new ParEstadoAccion(2, "C");
 			matriz[2][char2int('_')] = new ParEstadoAccion(2, "C");
 			
@@ -104,63 +99,60 @@ public class Compilador {
 		
 		}
 		
-		public int char2int(char c) {
-			switch (c) {
-			case '|':
-				return 0;
-			//TODO: Poner cada letra...
-			case 'l':  
+		public static int char2int(char c) {
+			if(((int)c >= 65 && (int)c<=90 )||((int)c >= 97 && (int)c<=122))
 				return 1;
-			case '0':  
-			case '1':  
-			case '2':  
-			case '3':  
-			case '4':  
-			case '5':  
-			case '6':
-			case '7':  
-			case '8':  
-			case '9':  
-				return 2;
-			case '\'':  
-				return 3;
-			case '/':
-				return 4;
-			case '_':  
-				return 5;
-			/*En el default
-			case 'c':  
-				return 6;*/
-			case '*':  
-				return 7;
-			//Poner todos los delimitadores?
-			case ' ':  
-			case '\t':
-			case '\n':
-				return 8;
-			case ';':  
-				return 9;
-			case '{':  
-				return 10;
-			case '}':  
-				return 11;
-			case '(':  
-				return 12;
-			case ')':  
-				return 13;
-			case '+':  
-				return 14;
-			case '<':  
-				return 15;
-			case '!':   
-				return 16;
-			case '=':  
-				return 17;
-			case ',':  
-				return 18;
-			//Caracteres (c)
-			default:
-				return 6;
+			else{
+				switch (c) {
+					case '|':
+						return 0;
+					case '0':  
+					case '1':  
+					case '2':  
+					case '3':  
+					case '4':  
+					case '5':  
+					case '6':
+					case '7':  
+					case '8':  
+					case '9':  
+						return 2;
+					case '\'':  
+						return 3;
+					case '/':
+						return 4;
+					case '_':  
+						return 5;
+					case '*':  
+						return 7;
+					case ' ':  
+					case '\t':
+					case '\n':
+						return 8;
+					case ';':  
+						return 9;
+					case '{':  
+						return 10;
+					case '}':  
+						return 11;
+					case '(':  
+						return 12;
+					case ')':  
+						return 13;
+					case '+':  
+						return 14;
+					case '<':  
+						return 15;
+					case '!':   
+						return 16;
+					case '=':  
+						return 17;
+					case ',':  
+						return 18;
+					//Caracteres (c)
+					default:
+						return 6;
+				}
 			}
 		}
 		
@@ -175,7 +167,7 @@ public class Compilador {
 		}
 	}
 	
-	public class Token<E>{
+	private static class Token<E>{
 		private String codToken;
 		private E atributo;
 		
@@ -205,7 +197,7 @@ public class Compilador {
 		
 	}
 	
-	public void ALex (){
+	public static void  ALex (){
 		int estado = 0;
 		while (estado < 8){
 			String accion = MT_AFD.accion(estado, car);
@@ -214,17 +206,71 @@ public class Compilador {
 				//error
 			}else{
 				switch(accion){
+					case "lee":
+						lee();
+						break;
+					case "A":
+						A();
+						break;
+					case "B":
+						B();
+						break;
+					case "C":
+						C();
+						break;
+					default:
+						switch(Integer.parseInt(accion.substring(1))){
+						case 1:
+							G1();
+							break;
+						case 2:
+							G2();
+							break;
+						case 3:
+							G3();
+							break;
+						case 4:
+							G4();
+							break;
+						case 5:
+							G5();
+							break;
+						case 6:
+							G6();
+							break;
+						case 7:
+							G7();
+							break;
+						case 8:
+							G8();
+							break;
+						case 9:
+							G9();
+							break;
+						case 10:
+							G10();
+							break;
+						case 11:
+							G11();
+							break;
+						case 12:
+							G12();
+							break;
+						case 13:
+							G13();
+							break;
+						case 14:
+							G14();
+					}
 					
 				}
 			}
-			
-			
 		}
 	}
 	
 	
-	//Acciones semánticas
-	public void lee(){
+	//Acciones semanticas
+	public static void lee(){
 		int caracterInt=-1;
 		try {
 			if((caracterInt=br.read())!=-1){
@@ -236,71 +282,128 @@ public class Compilador {
 		}
 	}
 	
-	public void error() {
+	public static void error() {
 		System.out.println("Error en transicion no prevista");
 	}
 	
-	public void A(){
+	private static void A(){
 		lee();
 		num = Character.getNumericValue(car);
 	}
 	
-	public void B(){
+	private static void B(){
 		lee();
 		num = num*10 + Character.getNumericValue(car);
 	}
 	
-	public void C(){
+	private static void C(){
 		lee();
 		lex += car;
 	}
 	
-	public Token<Integer> G1(){
+	private static Token<Integer> G1(){
 		lee();
 		return new Token<Integer>("ASIGOR");
+		
 	}
 	
 	//palRes es un conjunto de Strings
-	public Token<Integer> G2(){
-		int p = -1;
-		if (palRes.contains(lex)) return new Token<Integer>(lex);
-		if ((p = buscaTS()) == -1) p = anadeTS();
-		return new Token<Integer>("ID", p);
+	private static Token<Integer> G2(){
+		switch(lex){
+			case "var":
+				return new Token<Integer>("DEC");
+			case "int":
+				return new Token<Integer>("TipoVarENT");
+			case "boolean":
+				return new Token<Integer>("TipoVarLOG");
+			case "string":
+				return new Token<Integer>("TipoVarCAD");
+			case "print":
+				return new Token<Integer>("Print");
+			case "input":
+				return new Token<Integer>("Input");
+			case "return":
+				return new Token<Integer>("Return");
+			case "function":
+				return new Token<Integer>("DECFunc");
+			case "if":
+				return new Token<Integer>("IF");
+			case "else":
+				return new Token<Integer>("ELSE");
+			default:
+				int p;
+				if ((p = buscaTS()) == -1) p = anadeTS();
+				return new Token<Integer>("ID", p);
+		}
 	}
 	
-	public Token<Integer> G3(){
+	private static Token<Integer> G3(){
 		if (num >= Math.pow(2, 15)) return new Token<Integer>("", -1); //ERROR
 		return new Token<Integer>("ENT", num);
 	}
 	
-	public Token<String> G4(){
+	private static Token<String> G4(){
 		lee();
 		return new Token<String>("CAD", lex);
 	}
-	
-	public void G5(){
-		//TODO del 5 al 14
+		
+	private static Token<Integer> G5(){
+		lee();
+		return new Token<Integer>("PuntoComa");
 	}
 	
-	public void G14() {
-		//TODO
+	private static Token<Integer> G6() {
+		lee();
+		return new Token<Integer>("CorcheteAbrir");
+	}
+	private static Token<Integer> G7() {
+		lee();
+		return new Token<Integer>("CorcheteCerrar");
+	}
+	private static Token<Integer> G8() {
+		lee();
+		return new Token<Integer>("ParentesisAbrir");
+	}
+	private static Token<Integer> G9() {
+		lee();
+		return new Token<Integer>("ParentesisCerrar");
+	}
+	private static Token<Integer> G10() {
+		lee();
+		return new Token<Integer>("SUMA");
+	}
+	private static Token<Integer> G11() {
+		lee();
+		return new Token<Integer>("MENOR");
+	}
+	private static Token<Integer> G12() {
+		lee();
+		return new Token<Integer>("NOT");
+	}
+	private static Token<Integer> G13() {
+		lee();
+		return new Token<Integer>("ASIG");
+	}
+	private static Token<Integer> G14() {
+		lee();
+		return new Token<Integer>("Coma");
 	}
 	
 	//Busca lex en TS
-	public int buscaTS() {
+	public static int buscaTS() {
 		//TODO
 		return 0;
 	}
 	
-	//Añade lex en TS
-	public int anadeTS() {
+	//Introduce lex en TS
+	public static int anadeTS() {
 		//TODO
 		return 0;
 	}
 	
 	
 	public static void main(String []args){
-		File file = new File("Path");
+		File file = new File(args[0]);
 		br = null;
 		try {
 			br = new BufferedReader(new FileReader(file));
@@ -308,15 +411,10 @@ public class Compilador {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		int caracterInt=-1;
-		try {
-			while((caracterInt=br.read())!=-1){
-				car = (char) caracterInt;
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		lee();
+		while(car!='\0')
+			ALex();
+		
 	}
 	
 }
