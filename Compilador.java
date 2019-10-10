e05c32efa9113c27308fdd5d7b6c93e4fb73117f
popi300 <33000879+popi300@users.noreply.github.com>
2019-10-10 14:45:38 +0200

    Meter 0

--- Compilador.java
+++ Compilador.java
@@ -451,7 +451,7 @@ public  class Compilador {
 	
 	private static Token<String> G4(){
 		lee();
-		return new Token<String>("CAD", lex);
+		return new Token<String>("CAD", lex + "\0");
 	}
 		
 	private static Token<Integer> G5(){
